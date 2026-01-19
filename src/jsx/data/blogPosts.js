05eabf99718048669f364ecd7eacf1a20cafce2d
Charlie Cooper <charliepeople427@gmail.com>
2026-01-19 13:35:13 -0800

    gacp

--- src/jsx/data/blogPosts.js
+++ src/jsx/data/blogPosts.js
@@ -3,40 +3,46 @@
 export const blogPosts = [
   {
       id: 1,
-      title: 'Maintainence and Truncated MUNI routes?',
-      description: 'Not every route path made available by MUNI is useful. Here\'s how I settled on displaying the data.',
-      tags: ['MUNI!', 'PostgreSQL','React','React-Leaflet'],
+      title: 'That\'s it! I\'m SCALING my MUNI App',
+      description: 'Before I can add all the awesome features I want this thing to be rock solid.',
+      tags: ['MUNI!', 'Planning'],
       date: "01/04/26",
-      slug: "routes1",
-      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit hendrerit metus eget venenatis. Pellentesque sit amet lectus nunc. Duis a dignissim sapien. Donec non odio sem. Donec ornare mauris a sem sagittis consectetur. Sed nec mollis sem. Nunc bibendum tortor quis arcu varius, ac aliquet est dapibus. Sed non ante ipsum. Nulla id aliquam orci, ac gravida justo. Suspendisse egestas elit ac est pulvinar tempus. Mauris turpis erat, varius sit amet sapien a, egestas pharetra mi. Aliquam faucibus, diam at dapibus interdum, turpis turpis vestibulum mauris, et auctor leo lorem a lectus. Aliquam pharetra viverra nisl, at luctus mi tristique in.
+      slug: "planning1",
+      content: `That's it, I'm suring up the foundations of this MUNI app. In less than a month's time (by mid february 2026) I want everything automated, logged and documented.
+      
+      It will require 6 core steps:
+      
+      1. Add Redis cache to enforce rate limiting and to store static data to minimize DB I/O
+      
+      2. Write SQLAlchemy models for entire database schema
 
-Quisque lorem justo, tincidunt vitae pulvinar id, sagittis quis libero. Quisque accumsan bibendum odio quis bibendum. Suspendisse in justo in magna aliquet laoreet. Suspendisse sed odio elit. Donec nec nibh nisi. Nulla porta eros a dui tempor elementum. Integer eleifend orci a lorem venenatis gravida. Nam in placerat erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis sed aliquet ligula, ac dignissim lacus.
+      3. Automate fetching MUNI Agency's ~3 month updated routes/shapes/trips/schedules
+      
+      4. Implement centralized logging for I/O w.r.t. database: 
+          a) success/timestamps/metrics of new weekly partitions being made
+          b) success/timestamps/metrics of weekly partitions being exported to S3
+          c) count of vehicles records per week
+          d) when MUNI Agency new routes are published and integrated into database
+          e) number of unique users/traffic per week
+          f) probably more...
+      
+      5. Research NGINX and Cloudflared Tunnel and decide on way to migrate off of Vercel
 
-Morbi ac lobortis metus. Integer dignissim a enim at sodales. Duis id nibh dictum, feugiat leo vel, bibendum tellus. Donec dolor arcu, lacinia nec viverra et, feugiat id dolor. Phasellus porttitor mauris id urna feugiat rhoncus. Nunc non tempor lectus. Integer mattis ex sit amet venenatis accumsan. Donec pellentesque felis aliquet libero rutrum semper. Aliquam auctor dapibus volutpat. Mauris eget laoreet quam, non pulvinar turpis. Integer a aliquet lorem.
+      6. Implement login and users with auth
 
-Nunc interdum laoreet nisi in varius. Morbi tortor mauris, faucibus ut mattis quis, scelerisque et nibh. Donec vulputate massa sit amet tempor blandit. Aenean ut maximus nulla, sit amet tristique lectus. In enim ante, tempus a urna ut, interdum lobortis est. Phasellus ac vulputate odio. Ut enim tellus, tincidunt eu nunc ut, consectetur blandit velit. Pellentesque ac neque posuere, porttitor libero in, pellentesque felis. Vestibulum condimentum nulla in aliquet tristique. Mauris at elit est. Morbi vehicula tellus nec ex suscipit aliquam. Quisque sit amet neque dolor. Maecenas consequat, quam sit amet vestibulum sodales, nulla diam convallis sem, vitae fringilla odio augue pellentesque justo. Nunc a tristique erat.
-
-Maecenas imperdiet, nunc vel dictum accumsan, ligula velit commodo lacus, facilisis dictum diam orci vel ligula. Aenean sapien nisl, auctor ut tempor eu, pharetra a tortor. Integer aliquam interdum lorem. Duis vel diam dolor. In tincidunt purus mauris, at tincidunt lorem facilisis sit amet. Vivamus in finibus turpis. Proin euismod, purus a efficitur tristique, leo magna ultricies diam, sit amet pretium leo leo a felis. Nam dignissim mi ac elit placerat egestas. Proin lacinia ullamcorper dolor, in posuere mauris ullamcorper id. Donec sem lacus, scelerisque quis ex at, semper interdum ligula. Vestibulum non pretium augue. Pellentesque luctus porta mauris. Sed id rhoncus diam.
-
-Etiam pretium vel ante a accumsan. Aliquam sapien velit, bibendum et erat in, condimentum maximus felis. Suspendisse maximus rhoncus velit in laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut risus quis sapien feugiat aliquam et non leo. Fusce rutrum velit augue, nec laoreet nisl malesuada vel. Vivamus sit amet bibendum leo, sed consectetur mauris.
-
-Nam eleifend mollis eros vel auctor. Sed nunc nisi, sodales in lorem id, varius vehicula eros. Ut dapibus tristique placerat. Aenean et mauris a ex feugiat maximus. Donec dignissim convallis purus, at sodales dolor eleifend in. Cras eleifend varius sapien, quis consectetur ex ornare vel. Nullam id tempor lacus. Proin congue lacinia ligula, blandit congue lorem porta id. Nunc varius pulvinar neque vel convallis. Curabitur ultricies accumsan arcu molestie pellentesque.
-
-Suspendisse rhoncus vulputate libero tempus imperdiet. Integer luctus augue ac neque blandit, eu euismod mi cursus. Donec tempor nibh mattis lacus lobortis laoreet. Aenean sit amet egestas felis. Pellentesque lacinia nisi at eros fermentum laoreet. Nulla eu commodo urna. Cras turpis justo, luctus sed felis at, pellentesque efficitur nulla. Integer efficitur ex posuere tincidunt posuere. Aenean a augue sodales, vulputate turpis quis, mollis dui. Aenean fermentum a tellus id cursus. Ut pulvinar sodales imperdiet. Nullam ultrices dolor vel vulputate aliquet. Morbi interdum odio id diam volutpat, eget suscipit velit elementum. Phasellus placerat augue sit amet metus sagittis, nec sodales turpis molestie. Maecenas neque ipsum, varius eu euismod in, eleifend non elit. Proin commodo nunc ac purus egestas, et ultrices enim hendrerit.
-
-Duis vitae faucibus magna. Integer augue diam, sagittis quis blandit nec, mollis eget erat. Fusce at rhoncus lectus. Proin accumsan nisl ac tempor vehicula. Vestibulum et tempus mi. Sed elementum, velit sit amet dapibus condimentum, diam tortor faucibus libero, non laoreet massa felis nec massa. Nulla vel metus eu nulla maximus bibendum. Mauris placerat pretium quam at eleifend. Pellentesque non suscipit nibh. In tristique purus massa, et porta dui lacinia vitae. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse interdum nibh nisi, sodales sagittis nulla auctor nec.
-
-Integer eget nisl vulputate, eleifend sem ut, congue dolor. Morbi pellentesque, lectus ut tincidunt fringilla, arcu turpis mattis risus, id venenatis magna libero in purus. Donec at enim ullamcorper, tincidunt turpis eget, euismod diam. Vivamus semper fringilla elit quis placerat. Quisque vehicula nec tortor at aliquam. Etiam iaculis varius odio, vitae egestas metus faucibus vel. Aenean pharetra lacinia risus, quis facilisis dui dapibus ut. Vivamus suscipit ante ut odio tempus, at dictum dolor commodo. Vivamus aliquam sapien ac lorem eleifend, et ornare lacus rutrum. Aliquam malesuada, risus sagittis iaculis elementum, erat massa efficitur ipsum, quis fermentum leo urna a magna. Praesent luctus nunc pharetra, sodales turpis eu, mollis odio. Sed vulputate tincidunt nulla, nec facilisis justo. Nulla placerat dapibus ipsum eu condimentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque efficitur ante id libero pharetra, ut commodo massa varius. Vivamus interdum ante sed elementum malesuada.`,
+      I have my work cut out for me. But luckily this shiiii is fun af.
+          `,
   },
   {
       id: 2,
-      title: 'Representing UI state in JSON for LLM integration.',
-      description: 'How much work to go from manual controls to LLM controled map?',
-      tags: ['MUNI!', 'Prompt-engineering', 'Claude', 'React', 'JSON'],
-      date: "01/04/26",
-      slug: "llm1",
-      content: `To be written...`,
+      title: 'Database Schema + Documentation',
+      description: 'Addressing pain points in MUNI App',
+      tags: ['MUNI!', 'SQLAlchemy', 'GTFS', 'PostgreSQL', 'Cron'],
+      date: "01/18/26",
+      slug: "planning1-0-1",
+      content: ""
   },
+  
 ];
 
 export default blogPosts;
